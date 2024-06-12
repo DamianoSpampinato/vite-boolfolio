8780de40983b20e55cb55affc69194c25327cc3a
DamianoSpampinato <ghgg585@gmail.com>
2024-06-12 21:17:32 +0200

    add button to show card

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -15,6 +15,7 @@ export default{
     <p v-if="project.summary" class="card-text">Description: {{ project.summary }}</p>
     <span v-if="project.type"> type: {{ project.type.name }}</span>
     <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+     <router-link :to="{name: 'project-show', params: {'slug' : project.slug}}" class="btn btn-primary">Vediamolo</router-link>
   </div>
 </div>
 </template>
