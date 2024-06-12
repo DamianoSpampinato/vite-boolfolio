b10a4b543e2fb400372471a586c77e6c0f1d34d9
DamianoSpampinato <ghgg585@gmail.com>
2024-06-12 21:13:11 +0200

    feat:add details to projectShow

--- src/pages/ProjectShow.vue
+++ src/pages/ProjectShow.vue
@@ -30,18 +30,21 @@ import axios from 'axios';
 
             <h1>Nome: {{ project.name }}</h1>
             <p>summary: {{ project.summary }}</p>
-        </div>
         <div v-if="project.type">
             Type: <strong>{{ project.type.name }}</strong>
         </div>
-        <div v-if="project.client_name">
-            {{ project.client_name }}
-        </div>
-        <div v-if="project.technologies">
-            <span v-for="technology in project.technologies" class="badge rounded-pill text-bg-primary m-1 ">{{ technology.name }}</span>
-
-        </div>
+    <div v-if="project.client_name">
+        {{ project.client_name }}
     </div>
+<div v-if="project.technologies.length > 0">
+    <span v-for="technology in project.technologies" class="badge rounded-pill text-bg-primary m-1 ">{{ technology.name }}</span>
+</div>
+<div v-if="project.img">
+    <img :src="`http://127.0.0.1:8000/${project.img}`" alt="non funziona il link">
+
+</div>
+</div>
+</div>
 </template>
 
 <style lang="scss">
